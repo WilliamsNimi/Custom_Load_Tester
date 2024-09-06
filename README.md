549a1b84462fc36b47ba60f7b70e0848b732c3b3
Nimi Williams <williamson.nimi@gmail.com>
2024-09-06 18:40:53 +0100

    Created new repo for load tester

---
+++ README.md
@@ -0,0 +1,4 @@
+This project is an attempt to build a custom HTTP Load testing tool.
+
+1. Run the requirements.txt file to install dependencies (python3 -r install requirments.txt)
+2. Run the tool (python3 get_reqs.py *Load int value* *URL*
---
+++ get_reqs.py
@@ -0,0 +1,52 @@
+import os
+import concurrent.futures
+import sys
+import time
+import datetime
+import requests
+from tabulate import tabulate
+
+start_time = datetime.datetime.now()
+success = 0
+failure = 0
+try:
+    url = sys.argv[2]
+except Exception as e:
+    with open("Error_Logs/LoadTesterErrorLog.txt", 'e') as f:
+        f.write(f"{datetime.datetime.now()}: {e}")
+try:          
+    load = sys.argv[1]
+    load = int(load)
+except Exception as e:
+    with open("Error_Logs/LoadTesterErrorLog.txt", 'a') as f:
+        f.write(f"{time.time()}: {e}")
+    print("Usage: Python3 get_reqs.py *Load int value* *URL*")
+
+def req(count):
+    global success, failure
+    #os.system(f"curl '{url}'")
+    try:
+        res = requests.get(url)
+        if res:
+            success = success + 1
+        elif res.status_code >= 400:
+            failure = failure + 1
+    except Exception as e:
+        failure = failure + 1
+        #Log Exception to a file
+def threading():
+    with concurrent.futures.ThreadPoolExecutor(max_workers = 1000) as executor:
+        executor.map(req, range(load))
+
+def display_table(success, failure):
+    table = [['Success', 'Failure'], [success, failure]]
+    return tabulate(table, headers='firstrow', tablefmt='fancy_grid')
+        
+if __name__ == "__main__":
+    try:
+        threading()
+    except NameError:
+        print("Please provide a valid load value. See Usage above")
+    end_time = datetime.datetime.now()
+    print(f"Time {end_time - start_time}")
+    print(display_table(success, failure))
